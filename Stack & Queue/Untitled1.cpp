becc24274d28454750e3b65773cb95e88d918051
Muzammil8166 <muzammilkureshi.in@gmail.com>
2026-03-20 12:42:30 +0530

    Initial DSA commit

---
+++ Stack & Queue/Untitled1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main (){
+	
+	int input,len;
+	int stack=[len],postion=-1;
+	printf("Enter the length of Stack");
+	scanf(%d,&len);
+	printf("Select a option\n\n");
+	printf("\n1.Push\n2.Pop\n3.Show\n4.Exit");
+	
+	switch(input){
+		case 1:{
+			printf("Enter Number to Push->");
+			
+			break;
+		}
+	}
+}
