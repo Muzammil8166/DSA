3ed43e2787a75f699d1716219678d9f3ec03c887
Muzammil8166 <muzammilkureshi.in@gmail.com>
2026-03-23 10:34:16 +0530

    Satck Program

--- Stack & Queue/Stack.cpp
+++ Stack & Queue/Stack.cpp
@@ -2,18 +2,54 @@
 
 int main (){
 	
-	int input,len;
-	int stack=[len],postion=-1;
-	printf("Enter the length of Stack");
-	scanf(%d,&len);
-	printf("Select a option\n\n");
-	printf("\n1.Push\n2.Pop\n3.Show\n4.Exit");
+	int len, input;
+	printf("Enter the length of Stack \n>>");
+	scanf("%d",&len);
+	int stack[len], top=-1, option, exit=1;
 	
-	switch(input){
-		case 1:{
-			printf("Enter Number to Push->");
-			
-			break;
+	do{	
+		printf("\nSelect a option");
+		printf("\n1.Push\n2.Pop\n3.Show\n4.Exit");
+		printf("\n>>");
+		scanf("%d",&option);
+		switch(option){
+			case 1:{
+				if(top == len-1){
+					printf("\nThe Stack is Full");
+				}else{
+					top++;
+					printf("\nEnter Number to Push >>");
+					scanf("%d", &input);
+					stack[top] = input;
+				}
+				break;
+			}
+			case 2:{
+				if(top == -1){
+					printf("\nStack is Empty");
+				}else{
+					printf("\n%d is Deleted",stack[top]);
+					top--;
+				}
+				break;
+				
+			}
+			case 3:{
+				if(top == -1){
+					printf("\nThe Stck is Empty");
+				}else{
+					for(int i=0; i <= top ; i++){
+						printf("\n%d at> %d",i+1,stack[i]);
+					}
+				}
+				
+				
+				break;
+			}
+			case 4:{
+				exit = 0;
+				break;
+			}
 		}
-	}
+	}while(exit);
 }
